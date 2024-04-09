1b678c725182d92aff813e62c7ab5e218a143f92
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-04-09 21:57:53 +0000

    Drata Compliance Recommendations (Grouped)

--- test-cases/terraform/aws/logging/elb_without_access_logs/main.tf
+++ test-cases/terraform/aws/logging/elb_without_access_logs/main.tf
@@ -3,7 +3,7 @@ resource "aws_elb" "test" {
   availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
   listener {
     instance_port     = 8000
-    instance_protocol = "http"
+    instance_protocol = "HTTPS"
     lb_port           = 80
     lb_protocol       = "HTTPS"
   }
