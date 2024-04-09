8c5b646959af122290bca4ca00c2f27def1154c1
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-04-09 21:57:52 +0000

    Drata Compliance Recommendations (Grouped)

--- test-cases/terraform/aws/best-practices/elasticache_automatic_backup/main.tf
+++ test-cases/terraform/aws/best-practices/elasticache_automatic_backup/main.tf
@@ -14,4 +14,5 @@ resource "aws_elasticache_cluster" "disabled" {
   parameter_group_name = "default.redis6.x"
 
   snapshot_retention_limit = 0
+  # Drata: Specify [aws_elasticache_cluster.snapshot_retention_limit] to ensure sensitive data is only available when necessary. Setting snapshot retention to 0 will disable automatic backups
 }
